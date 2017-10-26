0eb1bc84794876b0a3315ba734b78870014ceb0d
beyond <2425822718@qq.com>
2017-10-26 17:30:55 +0800

    是否

---
+++ img/id.js
@@ -0,0 +1,1 @@
+if(s_c_il[0]._setAnalyticsFields)s_c_il[0]._setAnalyticsFields({"mid":"08525353653588934211919382364996090523"});
---
+++ img/monitor.js
@@ -0,0 +1,40 @@
+var live800_companyID="155503";
+var live800_protocol="http:";
+var live800_isMobile="0";
+var jsessionId = ";jsessionid=F964F6FD085EBADE724615A0845D2BD2";
+try{
+if(navigator.cookieEnabled && navigator.platform !== "iPhone"){jsessionId = "";}
+}catch(e){}
+var enterurl = null;
+enterurl="https%3A%2F%2Fwww.nbastore.cn%2F";
+var isOldSkin=false;
+var server_prefix_list=['http://st8.live800.com/live800','http://st.live800.com/live800','http://st10.live800.com/live800','http://st16.live800.com/live800'];
+var isNeedCheckDomainBinding=false;
+var globalWindowAttribute='toolbar=0,scrollbars=0,location=0,menubar=0,resizable=1,width=920,height=620';
+live800_chatVersion="5";
+jid="3547095935";
+var live800_baseUrl="chat8.live800.com";
+var live800_baseHtmlUrl="chat8.live800.com";
+var live800_baseWebApp="/live800";
+var live800_baseChatHtmlDir="/chatClient";
+live800_Language="zh";
+monitor_live800_configID="149200";
+var live800_status="1";
+var monitorInterval=15000;
+live800_chatSkinColor ="#006ab6";
+monitor_live800_configContent="live800_welcome_switch=0&pageChatVisible=0&live800_invite_style=31&live800_invite_top=0&sideSpace=50&posSide=right&live800_invite_title=%26%2336992%3B%26%2335831%3B";
+live800_inviteContent="&#35831;&#38382;&#26377;&#20160;&#20040;&#21487;&#20197;&#24110;&#24744;?";
+live800_visitorAddr="&#27426;&#36814;&#24744;&#65292;&#26469;&#33258; &#21271;&#20140;&#24066; &#30340;&#26379;&#21451;";
+var invite_accept='&#30830;&#12288;&#23450;';
+var invite_cancel='&#21462;&#12288;&#28040;';
+var innerpage_chat='&#28857;&#20987;&#21672;&#35810;';
+var innerpage_leavemessage='&#28857;&#20987;&#30041;&#35328;';
+var default_exit_words='&#24744;&#30830;&#23450;&#24744;&#35201;&#36864;&#20986;&#21527;?';
+var leavewordEnable=0;var isChatWindowViaSSL=false;
+(function(){
+var live800Script = document.createElement('script');
+live800Script.type = 'text/javascript';
+var proto = 'https:' == document.location.protocol ? 'https://': 'http://';
+live800Script.src  = proto+'st.live800.com/live800/chatClient/script/monitorStatic5.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(live800Script, s);
+})();
